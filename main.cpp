db81b711bd0b3d3650146e6ec1d460eb2175f95d
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-10-18 17:18:47 +0900

    06-2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int all_files;
+static all_files;
 
 extern void sub();
 
